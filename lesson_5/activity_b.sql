60b0f86adc783b43a1bde328418441b2b8ab7783
Anthony Nandaa <profnandaa@gmail.com>
2018-08-22 20:00:09 +0300

    fix: spacing

--- lesson_5/activity_b.sql
+++ lesson_5/activity_b.sql
@@ -6,7 +6,7 @@ SELECT s.name, es.score
 FROM exam_score es
 JOIN student s ON es.student_id = s.id
 WHERE es.score = (
-      SELECT MAX(score) FROM exam_score
+  SELECT MAX(score) FROM exam_score
 ) OR es.score = (
   SELECT MIN(score) FROM exam_score
 );
@@ -24,8 +24,8 @@ GROUP BY s.id;
 -- step (5)
 
 SELECT
-      s.name as subject,
-      AVG(esc.score) as average_score
+  s.name as subject,
+  AVG(esc.score) as average_score
 FROM subject s
 JOIN exam_subject es ON s.id = es.subject_id
 JOIN exam_score esc ON esc.exam_subject_id = es.id
